aa8754839527bf8e3b1a5b4e9c28c450747769ca
Dima22848 <4918bolia@ogasa.org.ua>
2025-04-22 15:06:38 +0300

    eleventh commit

--- backend/alcoland/alcoland/asgi.py
+++ backend/alcoland/alcoland/asgi.py
@@ -3,11 +3,12 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import chat.routing  # Маршруты WebSocket из приложения chat
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alcoland.settings')
 print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
 django.setup()
+import chat.routing  # Маршруты WebSocket из приложения chat
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
